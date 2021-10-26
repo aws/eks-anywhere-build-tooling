4c56ee4002d43e63d57362bda29c85aa20675886
Jackson West <jgw@amazon.com>
2021-10-26 13:46:27 -0500

    etcadm: convert to using common.mk

--- projects/kubernetes-sigs/etcdadm/build/create_binaries.sh
+++ projects/kubernetes-sigs/etcdadm/build/create_binaries.sh
@@ -13,45 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO="${1?Specify first argument - repository name}"
-CLONE_URL="${2?Specify second argument - git clone endpoint}"
-TAG="${3?Specify third argument - git version tag}"
-GOLANG_VERSION="${4?Specify fourth argument - golang version}"
-BIN_ROOT="_output/bin"
-BIN_PATH=$BIN_ROOT/$REPO
+TAG="$1"
+BIN_PATH="$2"
+OS="$3"
+ARCH="$4"
 
-MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
-source "${MAKE_ROOT}/../../../build/lib/common.sh"
-
-function build::etcdadm::build_binaries(){
-  platform=$1
-  OS="$(cut -d '/' -f1 <<< ${platform})"
-  ARCH="$(cut -d '/' -f2 <<< ${platform})"
-  CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH go build -a -ldflags "-s -w -buildid= -extldflags '-static'" -o bin/etcdadm .
-  mkdir -p ../${BIN_PATH}/${OS}-${ARCH}/
-  mv bin/* ../${BIN_PATH}/${OS}-${ARCH}/
-  make clean
-}
-
-function build::etcdadm::binaries(){
-  mkdir -p $BIN_PATH
-  git clone $CLONE_URL $REPO
-  cd $REPO
-  build::common::wait_for_tag $TAG
-  git checkout $TAG
-  git apply --verbose $MAKE_ROOT/patches/*
-  build::common::use_go_version $GOLANG_VERSION
-  go mod vendor
-  build::etcdadm::build_binaries "linux/amd64"
-  build::gather_licenses $MAKE_ROOT/_output "."
-  cd ..
-  rm -rf $REPO
-}
-
-build::etcdadm::binaries
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -a -ldflags "-s -w -buildid= -extldflags '-static'" -o $BIN_PATH/etcdadm .
