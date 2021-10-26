87351cab5a43e63e6d3693f3932fca7eea745f58
Jackson West <jgw@amazon.com>
2021-10-26 15:01:55 -0500

    etcd-controllers: convert to using common.mk

--- projects/mrajashree/etcdadm-bootstrap-provider/build/create_binaries.sh
+++ projects/mrajashree/etcdadm-bootstrap-provider/build/create_binaries.sh
@@ -13,68 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 set -x
 set -o errexit
+set -o nounset
 set -o pipefail
 
-REPO="${1?Specify first argument - repository name}"
-CLONE_URL="${2?Specify second argument - git clone endpoint}"
-TAG="${3?Specify third argument - git version tag}"
-GOLANG_VERSION="${4?Specify fourth argument - golang version}"
-IMAGE_REPO="${5?Specify fifth argument - ecr image repo}"
-IMAGE_TAG="${6?Specify sixth argument - ecr image tag}"
-BIN_ROOT="_output/bin"
-BIN_PATH=$BIN_ROOT/$REPO
+TAG="$1"
+BIN_PATH="$2"
+OS="$3"
+ARCH="$4"
 
-MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
-source "${MAKE_ROOT}/../../../build/lib/common.sh"
-
-KUSTOMIZE_BIN="${MAKE_ROOT}/_output/kustomize-bin"
-
-function build::install::kustomize(){
-  curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh" | bash
-  mv kustomize $KUSTOMIZE_BIN
-  export PATH=$KUSTOMIZE_BIN:$PATH
-}
-
-function build::etcdadm-bootstrap-provider::create_binaries(){
-  platform=${1}
-  OS="$(cut -d '/' -f1 <<< ${platform})"
-  ARCH="$(cut -d '/' -f2 <<< ${platform})"
-  CGO_ENABLED=0 GO111MODULE=on GOOS=$OS GOARCH=$ARCH go build -v -o bin/manager -ldflags "-s -w -buildid=''" $(pwd)
-  mkdir -p ../${BIN_PATH}/${OS}-${ARCH}/
-  mv bin/* ../${BIN_PATH}/${OS}-${ARCH}/
-}
-
-function build::etcdadm-bootstrap-provider::manifests(){
-  MANIFEST_IMAGE_OVERRIDE="${IMAGE_REPO}/mrajashree/etcdadm-bootstrap-provider:${IMAGE_TAG}"
-  KUBE_RBAC_PROXY_IMAGE_OVERRIDE=${IMAGE_REPO}/brancz/kube-rbac-proxy:latest
-
-  sed -i "s,\${ETCDADM_BOOTSTRAP_IMAGE},${MANIFEST_IMAGE_OVERRIDE}," ./config/manager/manager.yaml
-  sed -i 's,image: .*,image: '"${KUBE_RBAC_PROXY_IMAGE_OVERRIDE}"',' ./config/default/manager_auth_proxy_patch.yaml
-
-  mkdir -p ../_output/manifests/bootstrap-etcdadm-bootstrap/${TAG}
-  kustomize build config/default > bootstrap-components.yaml
-  cp bootstrap-components.yaml "../_output/manifests/bootstrap-etcdadm-bootstrap/${TAG}"
-  cp ../manifests/metadata.yaml "../_output/manifests/bootstrap-etcdadm-bootstrap/${TAG}"
-}
-
-function build::etcdadm-bootstrap-provider::binaries(){
-  mkdir -p $BIN_PATH
-  mkdir $KUSTOMIZE_BIN
-  git clone $CLONE_URL $REPO
-  cd $REPO
-  build::common::wait_for_tag $TAG
-  git checkout $TAG
-  build::install::kustomize
-  build::common::use_go_version $GOLANG_VERSION
-  go mod vendor
-  build::etcdadm-bootstrap-provider::create_binaries "linux/amd64"
-  build::etcdadm-bootstrap-provider::manifests
-  build::gather_licenses $MAKE_ROOT/_output "."
-  cd ..
-  rm -rf $REPO
-}
-
-build::etcdadm-bootstrap-provider::binaries
+CGO_ENABLED=0 GO111MODULE=on GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -a -ldflags "-s -w -buildid=" -o $BIN_PATH/manager .
