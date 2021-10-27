88d7476f425238eecc559c786a75eab445de6646
Jackson West <jaxesn@gmail.com>
2021-10-27 17:26:54 -0500

    cert-manager: switch to using common.mk (#114)
    * cert-manager: switch to using common.mk
    
    * fix checksums

--- projects/jetstack/cert-manager/build/create_binaries.sh
+++ projects/jetstack/cert-manager/build/create_binaries.sh
@@ -13,61 +13,24 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 set -x
 set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO="${1?Specify first argument - repository name}"
-CLONE_URL="${2?Specify second argument - git clone endpoint}"
-TAG="${3?Specify third argument - git version tag}"
-GOLANG_VERSION="${4?Specify fourth argument - golang version}"
-BIN_ROOT="_output/bin"
-BIN_PATH=$BIN_ROOT/$REPO
+TAG="$1"
+BIN_PATH="$2"
+OS="$3"
+ARCH="$4"
 
-MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
-source "${MAKE_ROOT}/../../../build/lib/common.sh"
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -ldflags "-s -w -buildid= -extldflags -static" -o $BIN_PATH/cert-manager-acmesolver ./cmd/acmesolver
 
-function build::cert-manager::cherry_pick(){
-  # The v1.1.0 cert-manager project repo references a 3rd-party fork of a fork of golang/crypto (https://github.com/meyskens/crypto).
-  # In a later commit (https://github.com/jetstack/cert-manager/commit/b5be5a8730a7307072c1d5d5fc6f3eb57b6018f4), this crypto fork
-  # was moved into the cert-manager repo and consumed from there. We are cherry-picking this commit so that we can use the changes
-  # introduced by it
-  git config --global user.email "eks-anywhere-bot@amazonaws.com"
-  git config --global user.name "EKS Anywhere Bot"
-  git cherry-pick -m 1 b5be5a8730a7307072c1d5d5fc6f3eb57b6018f4 --strategy-option theirs
-}
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -ldflags "-s -w -buildid= -extldflags -static" -o $BIN_PATH/cert-manager-cainjector ./cmd/cainjector
 
-function build::cert-manager::build_binaries(){
-  platform=$1
-  OS="$(cut -d '/' -f1 <<< ${platform})"
-  ARCH="$(cut -d '/' -f2 <<< ${platform})"
-  export CGO_ENABLED=0
-  export GOARCH=$ARCH
-  export GOOS=$OS
-  go build -ldflags "-s -w -extldflags -static" -o bin/cert-manager-acmesolver -v ./cmd/acmesolver
-  go build -ldflags "-s -w -extldflags -static" -o bin/cert-manager-cainjector -v ./cmd/cainjector
-  go build -ldflags "-s -w -extldflags -static" -o bin/cert-manager-controller -v ./cmd/controller
-  go build -ldflags "-s -w -extldflags -static" -o bin/cert-manager-webhook -v ./cmd/webhook
-  mkdir -p ../${BIN_PATH}/${OS}-${ARCH}/
-  mv bin/* ../${BIN_PATH}/${OS}-${ARCH}/
-}
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -ldflags "-s -w -buildid= -extldflags -static" -o $BIN_PATH/cert-manager-controller ./cmd/controller
 
-function build::cert-manager::binaries(){
-  mkdir -p $BIN_PATH
-  git clone $CLONE_URL $REPO
-  cd $REPO
-  build::common::wait_for_tag $TAG
-  git checkout $TAG
-  build::cert-manager::cherry_pick
-  build::common::use_go_version $GOLANG_VERSION
-  go mod vendor
-  build::cert-manager::build_binaries "linux/amd64"
-  build::gather_licenses $MAKE_ROOT/_output "./cmd/acmesolver ./cmd/cainjector ./cmd/controller ./cmd/webhook"
-  
-  cd ..
-  rm -rf $REPO
-}
-
-build::cert-manager::binaries
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -ldflags "-s -w -buildid= -extldflags -static" -o $BIN_PATH/cert-manager-webhook ./cmd/webhook
