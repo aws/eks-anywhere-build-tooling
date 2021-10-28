57fc44beb30bf4e236154d4106d3f10339a88d36
Jackson West <jgw@amazon.com>
2021-10-28 09:20:59 -0500

    flux-controllers: switch to common.mk, excludes source-controller

--- projects/fluxcd/notification-controller/build/create_binaries.sh
+++ projects/fluxcd/notification-controller/build/create_binaries.sh
@@ -19,49 +19,10 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-REPO="${1?Specify first argument - repository name}"
-CLONE_URL="${2?Specify second argument - git clone endpoint}"
-TAG="${3?Specify third argument - git version tag}"
-GOLANG_VERSION="${4?Specify fourth argument - golang version}"
-BIN_ROOT="_output/bin"
-BIN_PATH=$BIN_ROOT/$REPO
+TAG="$1"
+BIN_PATH="$2"
+OS="$3"
+ARCH="$4"
 
-MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
-source "${MAKE_ROOT}/../../../build/lib/common.sh"
-
-function build::notification-controller::fix_licenses(){
-  # The notification-controller project consumes an older version (v2.3.0) of the k0kubun/pp module 
-  # which does not have a LICENSE file committed to the repo. Hence we are fetching the license file 
-  # from the master branch so that it is available in the vendor directory for go-licenses to pick up
-  wget https://raw.githubusercontent.com/k0kubun/pp/master/LICENSE.txt
-  mv LICENSE.txt ./vendor/github.com/k0kubun/pp/LICENSE.txt
-  
-  # Internal go.mod under /api directory
-  cp LICENSE ./vendor/github.com/fluxcd/notification-controller/api/LICENSE
-}
-
-function build::notification-controller::create_binaries(){
-  platform=${1}
-  OS="$(cut -d '/' -f1 <<< ${platform})"
-  ARCH="$(cut -d '/' -f2 <<< ${platform})"
-  CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH go build -a -ldflags "-s -w -buildid= -extldflags '-static'" -o bin/notification-controller .
-  mkdir -p ../${BIN_PATH}/${OS}-${ARCH}/
-  mv bin/* ../${BIN_PATH}/${OS}-${ARCH}/
-}
-
-function build::notification-controller::binaries(){
-  mkdir -p $BIN_PATH
-  git clone $CLONE_URL $REPO
-  cd $REPO
-  build::common::wait_for_tag $TAG
-  git checkout $TAG
-  build::common::use_go_version $GOLANG_VERSION
-  go mod vendor
-  build::notification-controller::create_binaries "linux/amd64"
-  build::notification-controller::fix_licenses
-  build::gather_licenses $MAKE_ROOT/_output "."
-  cd ..
-  rm -rf $REPO
-}
-
-build::notification-controller::binaries
+CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
+  go build -trimpath -a -ldflags "-s -w -buildid= -extldflags '-static'" -o $BIN_PATH/notification-controller .
