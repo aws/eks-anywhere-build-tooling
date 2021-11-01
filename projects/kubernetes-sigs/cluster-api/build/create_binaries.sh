0b89a86828fcc2aff5c10cd9069010ab6f5a3fbc
Jackson West <jgw@amazon.com>
2021-11-01 08:38:35 -0500

    common.mk: handle patches in common.mk instead of project makefiles

--- projects/kubernetes-sigs/cluster-api/build/create_binaries.sh
+++ projects/kubernetes-sigs/cluster-api/build/create_binaries.sh
@@ -22,24 +22,22 @@ TAG="$1"
 BIN_PATH="$2"
 OS="$3"
 ARCH="$4"
-
-# Fix source date in order to have reproducible builds
-export SOURCE_DATE_EPOCH="1635525388"
+BUILD_LIB_DIR="$5"
 
 # Set build time variables including version details
-VERSION_LDFLAGS=$(./hack/version.sh)
+VERSION_LDFLAGS=$($BUILD_LIB_DIR/version.sh sigs.k8s.io/cluster-api/cmd/version)
 
 CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
-	go build -trimpath -ldflags "-s -w -buildid=''" -o $BIN_PATH/manager sigs.k8s.io/cluster-api
+	go build -trimpath -ldflags "-s -w -buildid= -extldflags -static $VERSION_LDFLAGS" -o $BIN_PATH/manager sigs.k8s.io/cluster-api
 
 CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
-	go build -trimpath -ldflags "-s -w -buildid=''" -o $BIN_PATH/kubeadm-bootstrap-manager sigs.k8s.io/cluster-api/bootstrap/kubeadm
+	go build -trimpath -ldflags "-s -w -buildid= -extldflags -static $VERSION_LDFLAGS" -o $BIN_PATH/kubeadm-bootstrap-manager sigs.k8s.io/cluster-api/bootstrap/kubeadm
 
 CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
-	go build -trimpath -ldflags "-s -w -buildid=''" -o $BIN_PATH/kubeadm-control-plane-manager sigs.k8s.io/cluster-api/controlplane/kubeadm
+	go build -trimpath -ldflags "-s -w -buildid= -extldflags -static $VERSION_LDFLAGS" -o $BIN_PATH/kubeadm-control-plane-manager sigs.k8s.io/cluster-api/controlplane/kubeadm
 
 CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
-	go build -trimpath -ldflags "-s -w -buildid='' $VERSION_LDFLAGS" -o $BIN_PATH/clusterctl sigs.k8s.io/cluster-api/cmd/clusterctl
+	go build -trimpath -ldflags "-s -w -buildid= -extldflags -static $VERSION_LDFLAGS" -o $BIN_PATH/clusterctl sigs.k8s.io/cluster-api/cmd/clusterctl
 
 (cd test/infrastructure/docker && go mod vendor && CGO_ENABLED=0 GOOS=$OS GOARCH=$ARCH \
-	go build -trimpath -ldflags "-s -w -buildid=''" -o $BIN_PATH/cluster-api-provider-docker-manager sigs.k8s.io/cluster-api/test/infrastructure/docker)
+	go build -trimpath -ldflags "-s -w -buildid= -extldflags -static" -o $BIN_PATH/cluster-api-provider-docker-manager sigs.k8s.io/cluster-api/test/infrastructure/docker)
