574fff413abc2e3a485a2d05340bdb1f3b9dd45f
Terry Howe <tlhowe@amazon.com>
2021-11-08 09:05:42 -0700

    Add PVC to test application

--- projects/aws/eks-anywhere-test/conf/entrypoint.sh
+++ projects/aws/eks-anywhere-test/conf/entrypoint.sh
@@ -3,6 +3,11 @@
 export foo="$(cat /config/foo | tr -d '\n')"
 export bar="$(cat /secrets/bar | tr -d '\n')"
 export version="$(cat /IMAGE_TAG | tr -d '\n')"
+export pvcsize="$(df --output=size /pvc | tail -1)"
+date=$(date +"%Y%m%d%H%M%S")
+date_value=$(date +"%Y-%m-%d %H:%M:%S")
+echo "${date_value}" >/pvc/$date
+export history="$(cat /pvc/* | tr -d '\n' ',')"
 
 mkdir -p /usr/share/nginx/txt/
 echo "⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢
@@ -31,15 +36,17 @@ https://anywhere.eks.amazonaws.com
 config value foo: ${foo}
 secret value bar: ${bar}
 version: ${version}
+pvc size 1K blocks: ${pvcsize}
+history: ${history}
 
 ⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢" \
 	| tee /usr/share/nginx/txt/index.html
 cat /usr/share/nginx/index.template | envsubst > /usr/share/nginx/html/index.html
 
-echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","version":"$version"}' \
+echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","pvcsize":"$pvcsize","version":"$version"}' \
     | envsubst > /usr/share/nginx/txt/index.json
 ln -s /usr/share/nginx/txt/index.json /usr/share/nginx/html/index.json
 
-export -n foo bar version
+export -n foo bar version pvcsize history
 
 exec "$@"
