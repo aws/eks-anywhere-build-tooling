26e40a822be0e8db74fce7db35071322266c5446
Abhay Krishna <arnchlm@amazon.com>
2021-11-08 17:09:30 -0800

    Install QEMU during build setup (#148)

--- build/lib/setup.sh
+++ build/lib/setup.sh
@@ -4,6 +4,8 @@ set -x
 set -e
 set -o pipefail
 
+source /docker.sh 
+
 CODEBUILD_CI="${CODEBUILD_CI:-false}"
 GIT_CONFIG_SCOPE="--global"
 if [[ "$CODEBUILD_CI" = "true" ]] && [[ "$CODEBUILD_BUILD_ID" =~ "aws-staging-bundle-build" ]]; then
@@ -17,3 +19,7 @@ fi
 mv config/docker-ecr-config.json /root/.docker/config.json
 git config ${GIT_CONFIG_SCOPE} credential.helper '!aws codecommit credential-helper $@'
 git config ${GIT_CONFIG_SCOPE} credential.UseHttpPath true
+
+start::dockerd
+wait::for::dockerd
+docker run --privileged --rm public.ecr.aws/eks-distro-build-tooling/binfmt-misc:qemu-v6.1.0 --install aarch64
