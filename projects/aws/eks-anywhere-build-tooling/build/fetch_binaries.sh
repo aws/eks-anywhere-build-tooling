7c702fa0b3fdaaad512ea07526f18e63c9641c6e
Jackson West <jaxesn@gmail.com>
2021-11-17 12:38:32 -0600

    eks-a-tools: switch to common.mk (#187)
    * eks-a-tools: switch to common.mk
    
    * fix

--- projects/aws/eks-anywhere-build-tooling/build/fetch_binaries.sh
+++ projects/aws/eks-anywhere-build-tooling/build/fetch_binaries.sh
@@ -22,9 +22,7 @@ set -o pipefail
 MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source "${MAKE_ROOT}/../../../build/lib/common.sh"
 
-OUTPUT_DIR="${MAKE_ROOT}/_output"
-EKS_A_TOOL_BINARY_DIR="${OUTPUT_DIR}/eks-a-tools/binary"
-EKS_A_TOOL_LICENSE_DIR="${OUTPUT_DIR}/eks-a-tools/licenses"
+ARTIFACTS_BUCKET="$1"
 
 declare -A repos=(["fluxcd/flux2"]="true"
                   ["kubernetes-sigs/cluster-api"]="true"
@@ -43,36 +41,48 @@ declare -A project_bin_licenses=(["flux2"]="flux FLUX2"
                                  ["govmomi"]="govc GOVMOMI")
 
 function unpack::tarballs(){
-  mkdir -p $OUTPUT_DIR
+  local -r output_dir="$1"
+  local -r arch="$2"
+
+  mkdir -p $output_dir
   for repo in "${!repos[@]}"
   do
     project=$repo
     base=$(basename $project)
-    mkdir $OUTPUT_DIR/$base
+    mkdir $output_dir/$base
     private=${repos[$repo]}
     if [ $private = "true" ]; then
-      URL=$(build::common::get_latest_eksa_asset_url $ARTIFACTS_BUCKET $project)
+      URL=$(build::common::get_latest_eksa_asset_url $ARTIFACTS_BUCKET $project $arch)
     else      
-      URL=$(build::eksd_releases::get_eksd_kubernetes_asset_url "kubernetes-client-linux-amd64.tar.gz")
+      URL=$(build::eksd_releases::get_eksd_kubernetes_asset_url "kubernetes-client-linux-$arch.tar.gz" $(build::eksd_releases::get_release_branch) $arch)
     fi
-    curl -sSL "${URL}" -o $OUTPUT_DIR/tmp.tar.gz
-    tar xzf $OUTPUT_DIR/tmp.tar.gz -C $OUTPUT_DIR/$base
+    curl -sSL "${URL}" -o $output_dir/tmp.tar.gz
+    tar xzf $output_dir/tmp.tar.gz -C $output_dir/$base
   done
 }
 
 function copy::binaries::licenses(){
-  mkdir -p $EKS_A_TOOL_BINARY_DIR
-  mkdir -p $EKS_A_TOOL_LICENSE_DIR
+  local -r output_dir="$1"
+  local -r eks_a_tool_binary_dir="$2"
+  local -r eks_a_tool_license_dir="$3"
+
+  mkdir -p $eks_a_tool_binary_dir
+  mkdir -p $eks_a_tool_license_dir
   for project in "${!project_bin_licenses[@]}"
   do
     bin_license_map=(${project_bin_licenses[$project]})
     binary=${bin_license_map[0]}
     license_prefix=${bin_license_map[1]}
-    cp ./_output/$project/$binary $EKS_A_TOOL_BINARY_DIR/$(basename $binary)
-    cp ./_output/$project/ATTRIBUTION.txt $EKS_A_TOOL_LICENSE_DIR/${license_prefix}_ATTRIBUTION.txt
-    cp -r ./_output/$project/LICENSES $EKS_A_TOOL_LICENSE_DIR/${license_prefix}_LICENSES
+    cp $output_dir/$project/$binary $eks_a_tool_binary_dir/$(basename $binary)
+    cp $output_dir/$project/ATTRIBUTION.txt $eks_a_tool_license_dir/${license_prefix}_ATTRIBUTION.txt
+    cp -r $output_dir/$project/LICENSES $eks_a_tool_license_dir/${license_prefix}_LICENSES
   done
 }
-
-unpack::tarballs
-copy::binaries::licenses
+for arch in amd64 arm64; do
+  OUTPUT_DIR="${MAKE_ROOT}/_output/linux-$arch"
+  EKS_A_TOOL_BINARY_DIR="${OUTPUT_DIR}/eks-a-tools/binary"
+  EKS_A_TOOL_LICENSE_DIR="${OUTPUT_DIR}/eks-a-tools/licenses"
+  
+  unpack::tarballs $OUTPUT_DIR $arch
+  copy::binaries::licenses $OUTPUT_DIR $EKS_A_TOOL_BINARY_DIR $EKS_A_TOOL_LICENSE_DIR
+done
