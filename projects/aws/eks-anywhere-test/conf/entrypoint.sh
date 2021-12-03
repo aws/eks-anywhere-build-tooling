f94d570305c2c5e012c966fd5ce8fd40de2555fa
Terry Howe <tlhowe@amazon.com>
2021-12-03 09:29:40 -0700

    Get rid of pvcsize

--- projects/aws/eks-anywhere-test/conf/entrypoint.sh
+++ projects/aws/eks-anywhere-test/conf/entrypoint.sh
@@ -3,11 +3,10 @@
 export foo="$(cat /config/foo | tr -d '\n')"
 export bar="$(cat /secrets/bar | tr -d '\n')"
 export version="$(cat /IMAGE_TAG | tr -d '\n')"
-export pvcsize="$(df --human-readable  --output=size /pvc | tail -1)"
 date=$(date +"%Y%m%d%H%M%S")
 date_value=$(date +"%Y-%m-%d %H:%M:%S")
 echo "${date_value}" >/pvc/$date
-export history="$(cat /pvc/* | tr -d '\n' ',')"
+export history="$(cat /pvc/* | tr '\n' ',')"
 
 mkdir -p /usr/share/nginx/txt/
 echo "⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢
@@ -36,17 +35,16 @@ https://anywhere.eks.amazonaws.com
 config value foo: ${foo}
 secret value bar: ${bar}
 version: ${version}
-pvc size: ${pvcsize}
 history: ${history}
 
 ⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢" \
 	| tee /usr/share/nginx/txt/index.html
 cat /usr/share/nginx/index.template | envsubst > /usr/share/nginx/html/index.html
 
-echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","pvcsize":"$pvcsize","version":"$version"}' \
+echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","version":"$version"}' \
     | envsubst > /usr/share/nginx/txt/index.json
 ln -s /usr/share/nginx/txt/index.json /usr/share/nginx/html/index.json
 
-export -n foo bar version pvcsize history
+export -n foo bar version history
 
 exec "$@"
