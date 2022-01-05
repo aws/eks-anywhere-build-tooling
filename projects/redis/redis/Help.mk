fe32eb5fd11b0f44534538f514cc22c8bddeb9a0
Terry Howe <tlhowe@amazon.com>
2022-01-05 06:02:25 -0700

    Add help file

---
+++ projects/redis/redis/Help.mk
@@ -0,0 +1,31 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `redis`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+patch-repo: ## Patch upstream repo with patches in patches directory
+
+##@ Image Targets
+local-images: ## Builds `redis/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `redis/images/push` to IMAGE_REPO
+redis/images/amd64: ## Builds/pushes `redis/images/amd64`
+redis/images/push: ## Builds/pushes `redis/images/push`
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `local-images`
+release: ## Called via prow postsubmit + release jobs, calls `images`
+########### END GENERATED ###########################
