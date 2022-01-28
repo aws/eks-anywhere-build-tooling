d16fa797e2a94cc8658ab26f5395a8101b05ae9d
Abhay Krishna Arunachalam <arnchlm@amazon.com>
2022-01-27 21:09:19 -0800

    Ensure EC2 keys get created with unique name to avoid collisions

--- projects/kubernetes-sigs/image-builder/build/build_raw_image.sh
+++ projects/kubernetes-sigs/image-builder/build/build_raw_image.sh
@@ -27,11 +27,19 @@ AMI_ID="${5?Specify fifth argument - AMI ID to create instance}"
 INSTANCE_TYPE="${6?Specify sixth argument - Instance type to create}"
 KEY_NAME="${7?Specify seventh argument - Key name to associate with instance}"
 
+CODEBUILD_CI="${CODEBUILD_CI:-false}"
+CI="${CI:-false}"
+
+if [ "$CODEBUILD_CI" = "true" ]; then
+    KEY_NAME="$KEY_NAME-$CODEBUILD_BUILD_ID"
+elif [ "$CI" = "true" ]; then
+    KEY_NAME="$KEY_NAME-$PROW_JOB_ID"
+fi
+
 REPO_NAME=$(basename $REPO_ROOT)
 KEY_LOCATION=$REPO_ROOT/$PROJECT_PATH/$KEY_NAME.pem
 IMAGE_BUILDER_MAKE_ROOT=$PROJECT_PATH/$IMAGE_BUILDER_DIR
 SSH_OPTS="-i $KEY_LOCATION -o StrictHostKeyChecking=no -o ConnectTimeout=120"
-CODEBUILD_CI="${CODEBUILD_CI:-false}"
 
 terminate_instance() {
     aws ec2 terminate-instances --instance-ids $1
@@ -53,18 +61,24 @@ if ! aws ec2 wait key-pair-exists --key-names=$KEY_NAME 2>/dev/null ; then
 fi
 chmod 600 $KEY_LOCATION
 
-# Create a single EC2 instance with provided instance type and AMI
-# Query the instance ID for use in future commands
-# Wait in loop until instance is running
 RUN_INSTANCE_EXTRA_ARGS=""
 if [ "$CODEBUILD_CI" = "true" ]; then
+    # Select a random subnet from Codebuild project's private subnet list
     SUBNET_ID_LIST=$RAW_IMAGE_BUILD_SUBNET_ID
     SUBNET_COUNT=$(echo $SUBNET_ID_LIST | awk -F\, '{print NF}')
     INDEX=$(($RANDOM % $SUBNET_COUNT))
     SUBNET_ID=$(cut -d',' -f${INDEX} <<< $SUBNET_ID_LIST)
+
+    # Define extra args to run the instance in the same subnet and use
+    # the same security group as Codebuild
     RUN_INSTANCE_EXTRA_ARGS="--subnet-id $SUBNET_ID --security-group-ids $RAW_IMAGE_BUILD_SECURITY_GROUP --associate-public-ip-address"
 fi
+
+# Create a single EC2 instance with provided instance type and AMI
+# Query the instance ID for use in future commands
 INSTANCE_ID=$(aws ec2 run-instances --count 1 --image-id=$AMI_ID --instance-type $INSTANCE_TYPE --key-name $KEY_NAME $RUN_INSTANCE_EXTRA_ARGS --query "Instances[0].InstanceId" --output text)
+
+# Wait in loop until instance is running
 aws ec2 wait instance-running --instance-ids $INSTANCE_ID
 
 # Get the public DNS of the instance to use as SSH hostname
@@ -73,6 +87,8 @@ REMOTE_HOST=ec2-user@$PUBLIC_DNS_NAME
 
 MAX_RETRIES=15
 
+# rsync might sometimes fail with flaky connection issues, so
+# implementing retry logic will make it more robust to flakes
 for i in $(seq 1 $MAX_RETRIES); do
     echo "Attempt $(($i))"
 
@@ -95,4 +111,5 @@ fi
 ssh $SSH_OPTS $REMOTE_HOST "sudo usermod -a -G kvm ec2-user; sudo chmod 666 /dev/kvm; sudo chown root:kvm /dev/kvm; PACKER_VAR_FILES='$PACKER_VAR_FILES' PACKER_FLAGS=-force PACKER_LOG=1 make build-raw-ubuntu-2004 -C /home/ec2-user/$REPO_NAME/$IMAGE_BUILDER_MAKE_ROOT"
 
 # Copy built raw image from the instance back into the CI build environment
+mkdir -p $REPO_ROOT/$IMAGE_BUILDER_MAKE_ROOT/output
 scp $SSH_OPTS $REMOTE_HOST:/home/ec2-user/$REPO_NAME/$IMAGE_BUILDER_MAKE_ROOT/output/*.gz $REPO_ROOT/$IMAGE_BUILDER_MAKE_ROOT/output/
