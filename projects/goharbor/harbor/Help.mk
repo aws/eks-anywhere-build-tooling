54c837687d0463e63ecd7163e638ecbf5af1d5dc
Vincent Ni <kangqini@amazon.com>
2022-02-07 21:23:12 -0800

    fix harbor images; split out registry binary to its own project

--- projects/goharbor/harbor/Help.mk
+++ projects/goharbor/harbor/Help.mk
@@ -9,17 +9,50 @@
 ##@ GIT/Repo Targets
 clone-repo:  ## Clone upstream `harbor`
 checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+patch-repo: ## Patch upstream repo with patches in patches directory
 
 ##@ Binary Targets
-binaries: ## Build all binaries: `harbor-core` for `linux/amd64 linux/arm64`
+binaries: ## Build all binaries: `harbor-core harbor-jobservice harbor-registryctl harbor-migrate-patch harbor-migrate harbor-exporter` for `linux/amd64 linux/arm64`
 _output/bin/harbor/linux-amd64/harbor-core: ## Build `_output/bin/harbor/linux-amd64/harbor-core`
+_output/bin/harbor/linux-amd64/harbor-jobservice: ## Build `_output/bin/harbor/linux-amd64/harbor-jobservice`
+_output/bin/harbor/linux-amd64/harbor-registryctl: ## Build `_output/bin/harbor/linux-amd64/harbor-registryctl`
+_output/bin/harbor/linux-amd64/harbor-migrate-patch: ## Build `_output/bin/harbor/linux-amd64/harbor-migrate-patch`
+_output/bin/harbor/linux-amd64/harbor-migrate: ## Build `_output/bin/harbor/linux-amd64/harbor-migrate`
+_output/bin/harbor/linux-amd64/harbor-exporter: ## Build `_output/bin/harbor/linux-amd64/harbor-exporter`
 _output/bin/harbor/linux-arm64/harbor-core: ## Build `_output/bin/harbor/linux-arm64/harbor-core`
+_output/bin/harbor/linux-arm64/harbor-jobservice: ## Build `_output/bin/harbor/linux-arm64/harbor-jobservice`
+_output/bin/harbor/linux-arm64/harbor-registryctl: ## Build `_output/bin/harbor/linux-arm64/harbor-registryctl`
+_output/bin/harbor/linux-arm64/harbor-migrate-patch: ## Build `_output/bin/harbor/linux-arm64/harbor-migrate-patch`
+_output/bin/harbor/linux-arm64/harbor-migrate: ## Build `_output/bin/harbor/linux-arm64/harbor-migrate`
+_output/bin/harbor/linux-arm64/harbor-exporter: ## Build `_output/bin/harbor/linux-arm64/harbor-exporter`
 
 ##@ Image Targets
-local-images: ## Builds `harbor-core/images/amd64` as oci tars for presumbit validation
-images: ## Pushes `harbor-core/images/push` to IMAGE_REPO
+local-images: ## Builds `harbor-db/images/amd64 harbor-portal/images/amd64 harbor-core/images/amd64 harbor-log/images/amd64 harbor-nginx/images/amd64 harbor-jobservice/images/amd64 harbor-registry/images/amd64 harbor-registryctl/images/amd64 harbor-redis/images/amd64 harbor-exporter/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `harbor-db/images/push harbor-portal/images/push harbor-core/images/push harbor-log/images/push harbor-nginx/images/push harbor-jobservice/images/push harbor-registry/images/push harbor-registryctl/images/push harbor-redis/images/push harbor-exporter/images/push` to IMAGE_REPO
+harbor-db/images/amd64: ## Builds/pushes `harbor-db/images/amd64`
+harbor-portal/images/amd64: ## Builds/pushes `harbor-portal/images/amd64`
 harbor-core/images/amd64: ## Builds/pushes `harbor-core/images/amd64`
+harbor-log/images/amd64: ## Builds/pushes `harbor-log/images/amd64`
+harbor-nginx/images/amd64: ## Builds/pushes `harbor-nginx/images/amd64`
+harbor-jobservice/images/amd64: ## Builds/pushes `harbor-jobservice/images/amd64`
+harbor-registry/images/amd64: ## Builds/pushes `harbor-registry/images/amd64`
+harbor-registryctl/images/amd64: ## Builds/pushes `harbor-registryctl/images/amd64`
+harbor-redis/images/amd64: ## Builds/pushes `harbor-redis/images/amd64`
+harbor-exporter/images/amd64: ## Builds/pushes `harbor-exporter/images/amd64`
+harbor-db/images/push: ## Builds/pushes `harbor-db/images/push`
+harbor-portal/images/push: ## Builds/pushes `harbor-portal/images/push`
 harbor-core/images/push: ## Builds/pushes `harbor-core/images/push`
+harbor-log/images/push: ## Builds/pushes `harbor-log/images/push`
+harbor-nginx/images/push: ## Builds/pushes `harbor-nginx/images/push`
+harbor-jobservice/images/push: ## Builds/pushes `harbor-jobservice/images/push`
+harbor-registry/images/push: ## Builds/pushes `harbor-registry/images/push`
+harbor-registryctl/images/push: ## Builds/pushes `harbor-registryctl/images/push`
+harbor-redis/images/push: ## Builds/pushes `harbor-redis/images/push`
+harbor-exporter/images/push: ## Builds/pushes `harbor-exporter/images/push`
+
+##@ Fetch Binary Targets
+_output/dependencies/linux-amd64/eksa/distribution/distribution: ## Fetch `_output/dependencies/linux-amd64/eksa/distribution/distribution`
+_output/dependencies/linux-arm64/eksa/distribution/distribution: ## Fetch `_output/dependencies/linux-arm64/eksa/distribution/distribution`
 
 ##@ Checksum Targets
 checksums: ## Update checksums file based on currently built binaries.
--- projects/goharbor/harbor/README.md
+++ projects/goharbor/harbor/README.md
@@ -1,9 +1,9 @@
-## **kube-vip**
+## **harbor**
 ![Version](https://img.shields.io/badge/version-v0.3.7-blue)
 ![Build Status](https://codebuild.us-west-2.amazonaws.com/badges?uuid=eyJlbmNyeXB0ZWREYXRhIjoiZVJQQTc0Vk8rcHlMR0hOYnllRGNmV0NsQTNLNGFaS2hLME1MUmgwYkxpVUFoL0V0WHZzbXVCV1owQ0FUTlF6RHg1WXhWRXZLRzNwN2d2LzZGUVJvZ0pRPSIsIml2UGFyYW1ldGVyU3BlYyI6Im9jQmZMa216aHZpYmdrWDYiLCJtYXRlcmlhbFNldFNlcmlhbCI6MX0%3D&branch=main)
 
-The [kube-vip project](https://github.com/plunder-app/kube-vip) provides High-Availability and load-balancing for both the controlplane and Kubernetes services. The idea behind kube-vip is a small self-contained Highly-Available option for all environments, especially Bare-Metal, Edge (ARM/Raspberry Pi), Virtualisation, etc. kube-vip provides both a floating or virtual IP address for Kubernetes clusters as well as load-balancing the incoming traffic to various controlplane replicas. It thus simplifies the building of HA Kubernetes clusters with minimal components and configurations.
+The [harbor project](https://github.com/goharbor/harbor) is an open source trusted cloud native registry project that stores, signs, and scans content. Harbor extends the open source Docker Distribution by adding the functionalities usually required by users such as security, identity and management. Having a registry closer to the build and run environment can improve the image transfer efficiency. Harbor supports replication of images between registries, and also offers advanced security features such as user management, access control and activity auditing.
 
-In EKS-A, kube-vip offers HA and load-balancing services for Kubernetes clusters on vSphere infrastructure.
+In EKS-A, harbor offers local cloud native registry service for Kubernetes clusters on vSphere infrastructure.
 
-You can find the latest version of this image [on ECR Public Gallery](https://gallery.ecr.aws/eks-anywhere/plunder-app/kube-vip).
+You can find the latest version of its images [on ECR Public Gallery](https://gallery.ecr.aws/eks-anywhere/harbor/).
