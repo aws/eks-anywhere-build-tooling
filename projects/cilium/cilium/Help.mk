797271f256f894ff435b5e972bda8622ce7b140f
Jackson West <jgw@amazon.com>
2022-03-04 14:30:57 -0600

    cilium: fix manifest generationt to use proper tag

---
+++ projects/cilium/cilium/Help.mk
@@ -0,0 +1,29 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `cilium`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+
+##@ Artifact Targets
+tarballs: ## Create tarballs by calling build/lib/simple_create_tarballs.sh unless SIMPLE_CREATE_TARBALLS=false, then tarballs must be defined in project Makefile
+s3-artifacts: # Prepare ARTIFACTS_PATH folder structure with tarballs/manifests/other items to be uploaded to s3
+upload-artifacts: # Upload tarballs and other artifacts from ARTIFACTS_PATH to S3
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `upload-artifacts`
+release: ## Called via prow postsubmit + release jobs, calls `upload-artifacts`
+########### END GENERATED ###########################
