e5602afd878f00b5b0ddb3d82b13b6504ddd9974
Johnson Pan <92060615+johnsonjnpan@users.noreply.github.com>
2022-03-11 11:34:00 -0800

    Add support for loading userdata from secretsmanager (#533)
    * Add support for loading userdata from secretsmanager
    
    * Update checksums
    
    * Refactor SecretsManagerService
    
    * Refactor ExternalUserdata as AWSSecretsManagerUserData
    
    * Fix Checksums
    
    * Fix Checksums

--- projects/aws/bottlerocket-bootstrap/pkg/utils/userdata.go
+++ projects/aws/bottlerocket-bootstrap/pkg/utils/userdata.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"context"
+	"encoding/base64"
 	"fmt"
 	"io/fs"
 	"io/ioutil"
@@ -11,11 +13,16 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/eks-anywhere-build-tooling/aws/bottlerocket-bootstrap/pkg/service"
+
 	"github.com/pkg/errors"
 	"gopkg.in/yaml.v2"
 )
 
-const userDataFile = "/.bottlerocket/host-containers/current/user-data"
+const (
+	userDataFile      = "/.bottlerocket/host-containers/current/user-data"
+	awsSecretsManager = "AWSSecretsManager"
+)
 
 type WriteFile struct {
 	Path        string
@@ -24,26 +31,93 @@ type WriteFile struct {
 	Content     string
 }
 
-type UserData struct {
-	WriteFiles []WriteFile `yaml:"write_files"`
-	RunCmd     string      `yaml:"runcmd"`
+type AWSSecretsManagerData struct {
+	Provider string
+	Prefix   string
+	Chunks   int
 }
 
-func ParseUserData() (*UserData, error) {
-	fmt.Println("Parsing userdata")
+type AWSSecretsManagerUserData struct {
+	// This field is set by the CAPI provider.
+	// It indicates whether this UserData is normal userdata, or a userdata that is stored remotely.
+	UserDataType   string                `yaml:"user_data_type"`
+	UserDataSource AWSSecretsManagerData `yaml:"secrets_manager_data"`
+}
+
+type UserData struct {
+	// This field is set by the CAPI provider.
+	// It indicates whether this UserData is normal userdata, or a userdata that is stored remotely.
+	UserDataType string      `yaml:"user_data_type"`
+	WriteFiles   []WriteFile `yaml:"write_files"`
+	RunCmd       string      `yaml:"runcmd"`
+}
+
+func ResolveUserData() (*UserData, error) {
+	fmt.Println("Reading userdata file")
 	// read userdata from the file
 	data, err := ioutil.ReadFile(userDataFile)
 	if err != nil {
 		return nil, errors.Wrap(err, "Error reading user data file")
 	}
+	return processUserData(data, service.NewSecretsManagerService())
+}
 
+func processUserData(data []byte, secretsManagerService service.SecretsManagerService) (*UserData, error) {
 	userData := &UserData{}
-	err = yaml.Unmarshal(data, userData)
+	err := yaml.Unmarshal(data, userData)
 	if err != nil {
 		return nil, errors.Wrap(err, "Error unmarshalling user data")
 	}
 	fmt.Printf("\n%+v\n", userData)
-	return userData, nil
+	if userData.UserDataType == awsSecretsManager {
+		// If this is a AWSSecretsManager typped UserData, parse it as AWSSecretsManagerUserData
+		fmt.Println("The loaded userdata is referecing an external userdata, loading it...")
+		awsSecretsManagerUserData := &AWSSecretsManagerUserData{}
+		err = yaml.Unmarshal(data, awsSecretsManagerUserData)
+		if err != nil {
+			return nil, errors.Wrap(err, "Error unmarshalling user data")
+		}
+		bootstrapUserData, err := loadUserDataFromSecretsManager(awsSecretsManagerUserData, secretsManagerService)
+		if err != nil {
+			fmt.Printf("Error loading userdata from SecretsManager: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Successfully loaded userdata from SecretsManager")
+		return bootstrapUserData, nil
+	} else {
+		return userData, nil
+	}
+}
+
+func loadUserDataFromSecretsManager(awsSecretsManagerUserData *AWSSecretsManagerUserData, secretManagerService service.SecretsManagerService) (*UserData, error) {
+	compressedCloudConfigBinary := []byte{}
+	for i := 0; i < awsSecretsManagerUserData.UserDataSource.Chunks; i++ {
+		secretName := fmt.Sprintf("%s-%d", awsSecretsManagerUserData.UserDataSource.Prefix, i)
+		secret, err := secretManagerService.GetSecretValue(context.TODO(), secretName)
+		if err != nil {
+			return nil, err
+		}
+		compressedCloudConfigBinary = append(compressedCloudConfigBinary, secret.SecretBinary...)
+		secretManagerService.DeleteSecret(context.TODO(), secretName)
+	}
+
+	uncompressedData, err := GUnzipBytes(compressedCloudConfigBinary)
+	if err != nil {
+		return nil, err
+	}
+	base64UserDataString := string(uncompressedData)
+	actualUserDataByte, err := base64.StdEncoding.DecodeString(base64UserDataString)
+
+	if err != nil {
+		return nil, err
+	}
+
+	acutalUserData := &UserData{}
+	err = yaml.Unmarshal(actualUserDataByte, acutalUserData)
+	if err != nil {
+		return nil, errors.Wrap(err, "Error unmarshalling user data")
+	}
+	return acutalUserData, nil
 }
 
 func WriteUserDataFiles(userData *UserData) error {
