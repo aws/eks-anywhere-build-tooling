a423cf7e0f9b378432bade9bec7e3281382cb7a9
jonahjon <jonahjones094@gmail.com>
2022-03-15 14:40:01 -0500

    merge changes
    Signed-off-by: jonahjon <jonahjones094@gmail.com>

--- projects/aws/cluster-api-provider-aws-snow/Help.mk
+++ projects/aws/cluster-api-provider-aws-snow/Help.mk
@@ -10,6 +10,12 @@
 clone-repo:  ## Clone upstream `cluster-api-provider-aws-snow`
 checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
 
+##@ Image Targets
+local-images: ## Builds `cluster-api-provider-aws-snow/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `cluster-api-provider-aws-snow/images/push` to IMAGE_REPO
+cluster-api-provider-aws-snow/images/amd64: ## Builds/pushes `cluster-api-provider-aws-snow/images/amd64`
+cluster-api-provider-aws-snow/images/push: ## Builds/pushes `cluster-api-provider-aws-snow/images/push`
+
 ##@ Artifact Targets
 tarballs: ## Create tarballs by calling build/lib/simple_create_tarballs.sh unless SIMPLE_CREATE_TARBALLS=false, then tarballs must be defined in project Makefile
 s3-artifacts: # Prepare ARTIFACTS_PATH folder structure with tarballs/manifests/other items to be uploaded to s3
@@ -24,6 +30,6 @@ help: ## Display this help
 add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
 
 ##@ Build Targets
-build: ## Called via prow presubmit, calls `validate-checksums  attribution upload-artifacts attribution-pr`
-release: ## Called via prow postsubmit + release jobs, calls `validate-checksums  upload-artifacts`
+build: ## Called via prow presubmit, calls `validate-checksums local-images attribution upload-artifacts attribution-pr`
+release: ## Called via prow postsubmit + release jobs, calls `validate-checksums images upload-artifacts`
 ########### END GENERATED ###########################
---
+++ projects/aws/cluster-api-provider-aws-snow/docker/linux/Dockerfile
@@ -0,0 +1,2 @@
+ARG BASE_IMAGE # cluster-api-provider-aws-snow
+FROM $BASE_IMAGE
