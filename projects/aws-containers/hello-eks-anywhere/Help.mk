a013b550fb92735470ccd523fb4b4358ab29a990
Terry Howe <tlhowe@amazon.com>
2022-04-13 12:05:13 -0600

    Add hello eks app to all projects (#694)

--- projects/aws-containers/hello-eks-anywhere/Help.mk
+++ projects/aws-containers/hello-eks-anywhere/Help.mk
@@ -5,8 +5,9 @@
 # To update call: make add-generated-help-block
 # This is added to help document dynamic targets and support shell autocompletion
 
+
 ##@ GIT/Repo Targets
-clone-repo:  ## Clone upstream `eks-anywhere-packages`
+clone-repo:  ## Clone upstream `hello-eks-anywhere`
 checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
 
 ##@ Image Targets
@@ -23,12 +24,19 @@ helm/push: ## Build helm chart and push to registry defined in IMAGE_REPO.
 
 ##@ Clean Targets
 clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
 
 ##@ Helpers
 help: ## Display this help
 add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
 
+##@Update Helpers
+run-target-in-docker: ## Run `MAKE_TARGET` using builder base docker container
+update-attribution-checksums-docker: ## Update attribution and checksums using the builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+generate: ## Update UPSTREAM_PROJECTS.yaml
+
 ##@ Build Targets
-build: ## Called via prow presubmit, calls `local-images`
-release: ## Called via prow postsubmit + release jobs, calls `images`
+build: ## Called via prow presubmit, calls `validate-checksums local-images attribution  attribution-pr`
+release: ## Called via prow postsubmit + release jobs, calls `validate-checksums images `
 ########### END GENERATED ###########################
