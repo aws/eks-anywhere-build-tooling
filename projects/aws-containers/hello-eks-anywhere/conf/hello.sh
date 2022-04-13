8c8394a3271bf673bd2f74d798ced57f62ad3036
Terry Howe <tlhowe@amazon.com>
2022-04-13 08:15:11 -0600

    Add config and version to hello app (#689)

--- projects/aws-containers/hello-eks-anywhere/conf/hello.sh
+++ projects/aws-containers/hello-eks-anywhere/conf/hello.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+export foo="$(cat /config/foo | tr -d '\n')"
+export bar="$(cat /secrets/bar | tr -d '\n')"
+export version="$(cat /IMAGE_TAG | tr -d '\n')"
+
 mkdir -p /usr/share/nginx/txt/
 echo "⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢
 
@@ -24,6 +28,18 @@ You have successfully deployed the hello-eks-a pod $POD_NAME
 For more information check out
 https://anywhere.eks.amazonaws.com
 
+config value foo: ${foo}
+secret value bar: ${bar}
+version: ${version}
+
 ⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢" \
 	| tee /usr/share/nginx/txt/index.html
 cat /usr/share/nginx/index.template | envsubst > /usr/share/nginx/html/index.html
+
+echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","version":"$version"}' \
+    | envsubst > /usr/share/nginx/txt/index.json
+ln -s /usr/share/nginx/txt/index.json /usr/share/nginx/html/index.json
+
+export -n foo bar version
+
+exec "$@"
