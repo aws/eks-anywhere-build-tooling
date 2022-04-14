c74984b62b5dc6a76c5b766118005ec97d78f401
Terry Howe <tlhowe@amazon.com>
2022-04-14 09:22:11 -0600

    Update hello-eks-anywhere to match upstream configs (#699)

--- projects/aws-containers/hello-eks-anywhere/conf/hello.sh
+++ projects/aws-containers/hello-eks-anywhere/conf/hello.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-export foo="$(cat /config/foo | tr -d '\n')"
-export bar="$(cat /secrets/bar | tr -d '\n')"
 export version="$(cat /IMAGE_TAG | tr -d '\n')"
 
 mkdir -p /usr/share/nginx/txt/
@@ -28,18 +26,16 @@ You have successfully deployed the hello-eks-a pod $POD_NAME
 For more information check out
 https://anywhere.eks.amazonaws.com
 
-config value foo: ${foo}
-secret value bar: ${bar}
+${TITLE}
+${SUBTITLE}
 version: ${version}
 
 ⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢⬡⬢" \
 	| tee /usr/share/nginx/txt/index.html
 cat /usr/share/nginx/index.template | envsubst > /usr/share/nginx/html/index.html
 
-echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","foo":"$foo","bar":"$bar","version":"$version"}' \
+echo '{"podname":"${POD_NAME}","nodename":"$NODE_NAME","title":"$TITLE","subtitle":"$SUBTITLE","version":"$version"}' \
     | envsubst > /usr/share/nginx/txt/index.json
 ln -s /usr/share/nginx/txt/index.json /usr/share/nginx/html/index.json
-
-export -n foo bar version
 
 exec "$@"
