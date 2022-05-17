633226bffb93c8a8928e58561681c913f46dae30
Vignesh Goutham Ganesh <vgg@amazon.com>
2022-05-17 11:17:50 -0700

    Skip checksum validation for bootconfig

--- Common.mk
+++ Common.mk
@@ -357,6 +357,7 @@ IMAGE_OS?=
 KUSTOMIZE_TARGET=$(OUTPUT_DIR)/kustomize
 GIT_DEPS_DIR?=$(OUTPUT_DIR)/gitdependencies
 SPECIAL_TARGET_SECONDARY=$(strip $(call FULL_FETCH_BINARIES_TARGETS, $(FETCH_BINARIES_TARGETS)) $(GO_MOD_DOWNLOAD_TARGETS))
+SKIP_CHECKSUM_VALIDATION?=false
 ####################################################
 
 #################### TARGETS FOR OVERRIDING ########
@@ -542,7 +543,9 @@ endif
 .PHONY: validate-checksums
 validate-checksums: $(BINARY_TARGETS)
 ifneq ($(strip $(BINARY_TARGETS)),)
+ifneq ($(SKIP_CHECKSUM_VALIDATION), true)
 	$(BASE_DIRECTORY)/build/lib/validate_checksums.sh $(MAKE_ROOT) $(PROJECT_ROOT) $(MAKE_ROOT)/$(OUTPUT_BIN_DIR) $(FAKE_ARM_BINARIES_FOR_VALIDATION)
+endif
 endif
 
 #### Image Helpers
