140040ca8ce4143278efa5f339cd17d92f5895a8
Sallam <seifsall@f84d899362a8.ant.amazon.com>
2022-08-04 10:22:21 -0400

    Helm chart build finished, modification to helm_push.sh to remove all non-digits prior to the first number

--- build/lib/helm_push.sh
+++ build/lib/helm_push.sh
@@ -28,7 +28,7 @@ HELM_DESTINATION_REPOSITORY="${2?Second argument is helm repository}"
 IMAGE_TAG="${3?Third argument is image tag}"
 OUTPUT_DIR="${4?Fourth arguement is output directory}"
 
-SEMVER="${IMAGE_TAG#[^0-9]}" # remove any leading non-digits
+SEMVER=$(echo "$IMAGE_TAG" | sed 's/^[^0-9]*//') # remove any leading non-digits
 
 HELM_DESTINATION_OWNER=$(dirname ${HELM_DESTINATION_REPOSITORY})
 CHART_NAME=$(basename ${HELM_DESTINATION_REPOSITORY})
