60ab0f1968db15d6b315198362366348d8ea4ca5
Vignesh Goutham Ganesh <vgg@amazon.com>
2022-08-16 17:35:20 -0700

    Typos and minor fixes for image-builder

--- projects/aws/image-builder/cmd/build.go
+++ projects/aws/image-builder/cmd/build.go
@@ -9,10 +9,6 @@ import (
 	"github.com/aws/eks-anywhere-build-tooling/image-builder/builder"
 )
 
-const (
-	artifactsBucket = "s3://projectbuildpipeline-857-pipelineoutputartifactsb-10ajmk30khe3f"
-)
-
 var bo = &builder.BuildOptions{}
 
 var buildCmd = &cobra.Command{
@@ -31,8 +27,7 @@ func init() {
 	buildCmd.Flags().StringVar(&bo.Os, "os", "", "Operating system to use for EKS-A node image")
 	buildCmd.Flags().StringVar(&bo.Hypervisor, "hypervisor", "", "Target hypervisor EKS-A node image")
 	buildCmd.Flags().StringVar(&bo.VsphereConfig, "vsphere-config", "", "Path to vSphere Config file")
-	buildCmd.Flags().StringVar(&bo.ReleaseChannel, "release-channel", "1-21", "EKS-D Release channel for node image. Can be 1-20, 1-21 or 1-22")
-	buildCmd.Flags().StringVar(&bo.ArtifactsBucket, "artifacts-bucket", artifactsBucket, "S3 bucket storing all EKS-D and EKS-A Artifacts")
+	buildCmd.Flags().StringVar(&bo.ReleaseChannel, "release-channel", "1-23", "EKS-D Release channel for node image. Can be 1-20, 1-21, 1-22 or 1-23")
 	buildCmd.Flags().BoolVar(&bo.Force, "force", false, "Force flag to clean up leftover files from previous execution")
 	if err := buildCmd.MarkFlagRequired("os"); err != nil {
 		log.Fatalf("Error marking flag as required: %v", err)
