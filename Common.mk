012f0899ad2e105ff714cf8d10dfe3730fbc2fb6
Ostosh, Ivy <ivyjin@amazon.com>
2022-08-29 15:10:58 -0500

    Remove helm/pull from Local_IMAGE_TARGETS and BUILD_TARGETS

--- Common.mk
+++ Common.mk
@@ -151,7 +151,7 @@ BUILD_OCI_TARS?=false
 LOCAL_IMAGE_WO_HELM_TARGETS=$(foreach image,$(IMAGE_NAMES),$(image)/images/amd64)
 IMAGE_WO_HELM_TARGETS=$(foreach image,$(IMAGE_NAMES),$(if $(filter true,$(BUILD_OCI_TARS)),$(call IMAGE_TARGETS_FOR_NAME,$(image)),$(image)/images/push))
 
-LOCAL_IMAGE_TARGETS=$(LOCAL_IMAGE_WO_HELM_TARGETS) $(if $(filter true,$(HELM_PULL)),helm/pull,) $(if $(filter true,$(HAS_HELM_CHART)),helm/build,)
+LOCAL_IMAGE_TARGETS=$(LOCAL_IMAGE_WO_HELM_TARGETS) $(if $(filter true,$(HAS_HELM_CHART)),helm/build,)
 IMAGE_TARGETS=$(IMAGE_WO_HELM_TARGETS) $(if $(filter true,$(HAS_HELM_CHART)),helm/push,)
 
 # If running in the builder base on prow or codebuild, grab the current tag to be used when building with cgo
@@ -385,7 +385,7 @@ SKIP_CHECKSUM_VALIDATION?=false
 ####################################################
 
 #################### TARGETS FOR OVERRIDING ########
-BUILD_TARGETS?=validate-checksums attribution $(if $(IMAGE_NAMES),local-images-wo-helm,) $(if $(filter true,$(HELM_PULL)),helm/pull,) $(if $(filter true,$(HAS_HELM_CHART)),helm/build,) $(if $(filter true,$(HAS_S3_ARTIFACTS)),upload-artifacts,) attribution-pr
+BUILD_TARGETS?=validate-checksums attribution $(if $(IMAGE_NAMES),local-images-wo-helm,) $(if $(filter true,$(HAS_HELM_CHART)),helm/build,) $(if $(filter true,$(HAS_S3_ARTIFACTS)),upload-artifacts,) attribution-pr
 RELEASE_TARGETS?=validate-checksums $(if $(IMAGE_NAMES),images-wo-helm,) $(if $(filter true,$(HAS_HELM_CHART)),helm/push,) $(if $(filter true,$(HAS_S3_ARTIFACTS)),upload-artifacts,)
 ####################################################
 
