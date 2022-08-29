bf8da3b398cc684bc2250dbf25a02fff69d8f56e
Ivy Ostosh <ivyjin@amazon.com>
2022-08-29 11:48:15 -0500

    Add ADOT package (#1222)
    * Add ADOT package
    
    * Refine ADOT Makefile
    
    * Add default namespace for ADOT
    
    * Update ADOT README
    
    * Update ADOT Makefile for IMAGE_NAMES
    
    IMAGE_NAMES override is needed; without, the Help.mk file will try to generate targets for images and those targets will fail at execution
    
    * Rename ADOT helm ECR repo
    
    * Update make update-digests target

---
+++ projects/aws-observability/aws-otel-collector/Help.mk
@@ -0,0 +1,41 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `aws-otel-collector`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+
+##@ Image Targets
+local-images: ## Builds `helm/build` as oci tars for presumbit validation
+images: ## Pushes `helm/push` to IMAGE_REPO
+helm/build: ## Builds/pushes `helm/build`
+helm/push: ## Builds/pushes `helm/push`
+
+##@ Helm Targets
+helm/build: ## Build helm chart
+helm/push: ## Build helm chart and push to registry defined in IMAGE_REPO.
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@Update Helpers
+run-target-in-docker: ## Run `MAKE_TARGET` using builder base docker container
+update-attribution-checksums-docker: ## Update attribution and checksums using the builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+generate: ## Update UPSTREAM_PROJECTS.yaml
+create-ecr-repos: ## Create repos in ECR for project images for local testing
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `helm/build`
+release: ## Called via prow postsubmit + release jobs, calls `helm/push`
+########### END GENERATED ###########################
