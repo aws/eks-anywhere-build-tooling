db43dca872b48c5deaff0f74338dba08c3b1f24e
Jonathan Meier <jwmeier@amazon.com>
2022-09-26 09:45:05 -0400

    Add metrics-server helm chart

---
+++ projects/kubernetes-sigs/metrics-server/Help.mk
@@ -0,0 +1,35 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `aws-otel-collector`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+
+##@ Helm Targets
+helm/build: ## Build helm chart
+helm/push: ## Build helm chart and push to registry defined in IMAGE_REPO.
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@Update Helpers
+run-target-in-docker: ## Run `MAKE_TARGET` using builder base docker container
+update-attribution-checksums-docker: ## Update attribution and checksums using the builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+generate: ## Update UPSTREAM_PROJECTS.yaml
+create-ecr-repos: ## Create repos in ECR for project images for local testing
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `helm/build`
+release: ## Called via prow postsubmit + release jobs, calls `helm/push`
+########### END GENERATED ###########################
