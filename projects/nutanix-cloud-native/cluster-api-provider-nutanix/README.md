bba5ae1fc30d0b9691e93108525d37d8d5af2e48
Sid Shukla <6081171+thunderboltsid@users.noreply.github.com>
2022-09-29 02:16:37 +0200

    Add build info for CAPX (#1313)
    * Add build info for CAPX
    
    CAPX is Nutanix's Cluster API provider.
    
    * Update common.sh
    
    Use go1.19 for gather licenses if present.
    
    * Change go-licenses version for 1.19
    
    * Reverted to 1.18 till base image is built with 1.19
    
    Ref:  https://github.com/aws/eks-distro-build-tooling/pull/484
    
    * fixed the typo in name of the file
    
    * fixed the typo in the name of the file
    
    * reverted the test fix
    
    * Update projects/nutanix-cloud-native/cluster-api-provider-nutanix/README.md
    
    Co-authored-by: Abhinav Pandey <abhinavmpandey08@gmail.com>
    
    Co-authored-by: Deepak Muley <deepak.muley@nutanix.com>
    Co-authored-by: Abhinav Pandey <abhinavmpandey08@gmail.com>

---
+++ projects/nutanix-cloud-native/cluster-api-provider-nutanix/README.md
@@ -0,0 +1,17 @@
+## **Cluster API Provider for Nutanix**
+![Version](https://img.shields.io/badge/version-v0.5.1-blue)
+
+The [Cluster API Provider for Nutanix (CAPX)](https://github.com/nutanix-cloud-native/cluster-api-provider-nutanix) is a the implementation of Cluster API for Nutanix.
+
+
+### Updating
+
+1. Review releases and changelogs in upstream [repo](https://github.com/nutanix-cloud-native/cluster-api-provider-nutanix) and decide on the new version.
+   Please review carefully and if there are questions about changes necessary to eks-anywhere to support the new version
+   and/or automatically update between eks-anywhere version reach out to @jaxesn, @pokearu or @abhinavmpandey08.
+2. Update the `GIT_TAG` file to have the new desired version based on the upstream release tags.
+3. Check the go.mod file to see if the golang version has changed when updating a version. Update the `GOLANG_VERSION` in `Makefile` to match the version upstream.
+4. Compare the old tag to the new, looking specifically for Makefile changes. If `release-manifests` target has changed in the Makefile, make the required changes in `create_manifests.sh`
+5. Update checksums and attribution using `make update-attribution-checksums-docker`.
+6. Update the version at the top of this Readme.
+7. Run `make generate` to update the UPSTREAM_PROJECTS.yaml file.
---
+++ projects/nutanix-cloud-native/cluster-api-provider-nutanix/build/create_manifests.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+# Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -x
+set -o errexit
+set -o pipefail
+
+REPO="$1"
+OUTPUT_DIR="$2"
+ARTIFACTS_PATH="$3"
+TAG="$4"
+IMAGE_REPO="$5"
+IMAGE_TAG="$6"
+GOLANG_VERSION="$7"
+
+MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+source "${MAKE_ROOT}/../../../build/lib/common.sh"
+
+build::common::use_go_version $GOLANG_VERSION
+
+cd $REPO
+
+make release-manifests \
+  RELEASE_DIR="out" \
+  IMG="${IMAGE_REPO}:${IMAGE_TAG}"
+
+mkdir -p $OUTPUT_DIR/manifests/infrastructure-nutanix/$TAG
+cp out/metadata.yaml "$OUTPUT_DIR/manifests/infrastructure-nutanix/$TAG"
+cp out/cluster-template.yaml "$OUTPUT_DIR/manifests/infrastructure-nutanix/$TAG"
+cp out/infrastructure-components.yaml "$OUTPUT_DIR/manifests/infrastructure-nutanix/$TAG"
+
+cp -rf $OUTPUT_DIR/manifests $ARTIFACTS_PATH
