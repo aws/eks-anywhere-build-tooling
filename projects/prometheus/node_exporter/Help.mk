a349228f33947101947e69e65cc03fb51ec18cba
Ivy Ostosh <ivyjin@amazon.com>
2022-11-04 09:07:09 -0500

    Add project prometheus node_exporter (#1512)
    * Add project prometheus node_exporter
    
    * Update Prometheus node_exporter Makefile
    
    Move the go.mod update to a patch
    
    * Update Prometheus node_exporter base image

---
+++ projects/prometheus/node_exporter/Help.mk
@@ -0,0 +1,51 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `node_exporter`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+
+##@ Binary Targets
+binaries: ## Build all binaries: `node_exporter` for `linux/amd64 linux/arm64`
+_output/bin/node_exporter/linux-amd64/node_exporter: ## Build `_output/bin/node_exporter/linux-amd64/node_exporter`
+_output/bin/node_exporter/linux-arm64/node_exporter: ## Build `_output/bin/node_exporter/linux-arm64/node_exporter`
+
+##@ Image Targets
+local-images: ## Builds `node_exporter/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `node_exporter/images/push` to IMAGE_REPO
+node_exporter/images/amd64: ## Builds/pushes `node_exporter/images/amd64`
+node_exporter/images/push: ## Builds/pushes `node_exporter/images/push`
+
+##@ Checksum Targets
+checksums: ## Update checksums file based on currently built binaries.
+validate-checksums: # Validate checksums of currently built binaries against checksums file.
+
+##@ License Targets
+gather-licenses: ## Helper to call $(GATHER_LICENSES_TARGETS) which gathers all licenses
+attribution: ## Generates attribution from licenses gathered during `gather-licenses`.
+attribution-pr: ## Generates PR to update attribution files for projects
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-repo: ## Removes source directory
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@Update Helpers
+run-target-in-docker: ## Run `MAKE_TARGET` using builder base docker container
+update-attribution-checksums-docker: ## Update attribution and checksums using the builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+generate: ## Update UPSTREAM_PROJECTS.yaml
+create-ecr-repos: ## Create repos in ECR for project images for local testing
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `validate-checksums attribution local-images   attribution-pr`
+release: ## Called via prow postsubmit + release jobs, calls `validate-checksums images  `
+########### END GENERATED ###########################
