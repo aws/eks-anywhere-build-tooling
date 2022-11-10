703f0ea446a5ed002245a1a97bdab63f48434006
Jacob Weinstock <jacobweinstock@users.noreply.github.com>
2022-11-10 13:25:45 -0700

    Update for Vlan support in Tinkerbell (#1529)
    * Update Tinkerbell Boots version:
    
    This new version provides VLAN tagging support.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Add CAPT patch for updating tink dependency:
    
    The updated tink version has the support needed
    for enabling VLAN tagging functionality.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Add patch for enabling vlan interface creation
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Update Tinkerbell Hook commit:
    
    This commit has the functionality for VLAN tagging
    and removes the need to do any patching in this repo.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Fix Hook build issues:
    
    Update Hook's makefile to play nice with
    build-tools container image.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Update hardware crd spec in tinkerbell helm chart:
    
    This adds the vlan id field needed for trunk vlan support.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Add License copy back in:
    
    It was commented out for testing.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    * Add hardware spec CAPI labels back.
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>
    
    Signed-off-by: Jacob Weinstock <jakobweinstock@gmail.com>

--- projects/tinkerbell/hook/Help.mk
+++ projects/tinkerbell/hook/Help.mk
@@ -9,23 +9,22 @@
 ##@ GIT/Repo Targets
 clone-repo:  ## Clone upstream `hook`
 checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
-patch-repo: ## Patch upstream repo with patches in patches directory
 
 ##@ Binary Targets
-binaries: ## Build all binaries: `bootkit tink-docker` for `linux/amd64 linux/arm64`
-_output/bin/hook/linux-amd64/bootkit: ## Build `_output/bin/hook/linux-amd64/bootkit`
-_output/bin/hook/linux-amd64/tink-docker: ## Build `_output/bin/hook/linux-amd64/tink-docker`
-_output/bin/hook/linux-arm64/bootkit: ## Build `_output/bin/hook/linux-arm64/bootkit`
-_output/bin/hook/linux-arm64/tink-docker: ## Build `_output/bin/hook/linux-arm64/tink-docker`
+binaries: ## Build all binaries: `hook-bootkit hook-docker` for `linux/amd64 linux/arm64`
+_output/bin/hook/linux-amd64/hook-bootkit: ## Build `_output/bin/hook/linux-amd64/hook-bootkit`
+_output/bin/hook/linux-amd64/hook-docker: ## Build `_output/bin/hook/linux-amd64/hook-docker`
+_output/bin/hook/linux-arm64/hook-bootkit: ## Build `_output/bin/hook/linux-arm64/hook-bootkit`
+_output/bin/hook/linux-arm64/hook-docker: ## Build `_output/bin/hook/linux-arm64/hook-docker`
 
 ##@ Image Targets
-local-images: ## Builds `bootkit/images/amd64 tink-docker/images/amd64 kernel/images/amd64` as oci tars for presumbit validation
-images: ## Pushes `bootkit/images/push tink-docker/images/push kernel/images/push` to IMAGE_REPO
-bootkit/images/amd64: ## Builds/pushes `bootkit/images/amd64`
-tink-docker/images/amd64: ## Builds/pushes `tink-docker/images/amd64`
+local-images: ## Builds `hook-bootkit/images/amd64 hook-docker/images/amd64 kernel/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `hook-bootkit/images/push hook-docker/images/push kernel/images/push` to IMAGE_REPO
+hook-bootkit/images/amd64: ## Builds/pushes `hook-bootkit/images/amd64`
+hook-docker/images/amd64: ## Builds/pushes `hook-docker/images/amd64`
 kernel/images/amd64: ## Builds/pushes `kernel/images/amd64`
-bootkit/images/push: ## Builds/pushes `bootkit/images/push`
-tink-docker/images/push: ## Builds/pushes `tink-docker/images/push`
+hook-bootkit/images/push: ## Builds/pushes `hook-bootkit/images/push`
+hook-docker/images/push: ## Builds/pushes `hook-docker/images/push`
 kernel/images/push: ## Builds/pushes `kernel/images/push`
 
 ##@ Checksum Targets
