e0c9870147ea5d74b8777220d8b321e484d9b9b6
Joey Wang <jiayiwang7@yahoo.com>
2022-11-29 10:38:59 -0500

    Add single primary DNI and static ip support in snow bottlerocket bootstrap container (#1613)

--- projects/aws/bottlerocket-bootstrap/pkg/providers/snow/system/network_test.go
+++ projects/aws/bottlerocket-bootstrap/pkg/providers/snow/system/network_test.go
@@ -1,50 +1,77 @@
-package system
+package system_test
 
 import (
 	"testing"
 
 	. "github.com/onsi/gomega"
+
+	"github.com/eks-anywhere-build-tooling/aws/bottlerocket-bootstrap/pkg/providers/snow/system"
 )
 
-const multiDNIs = `version = 2
-[1]
-dhcp4 = true
-[2]
-dhcp4 = true
-[3]
-dhcp4 = true
-[eth0.static4]
-addresses = ["1.2.3.4/25"]
-[[eth0.route]]
-to = "metadata/32"
-from = "1.2.3.4"
-via = "gateway"
-`
-
-func TestGenerateUserData(t *testing.T) {
-	g := NewWithT(t)
-
-	testcases := []struct {
-		name   string
-		data   map[string]interface{}
-		output string
-	}{
-		{
-			name: "multi dnis",
-			data: map[string]interface{}{
-				"dnis":              []string{"1", "2", "3"},
-				"instanceIP":        "1.2.3.4",
-				"defaultGateway":    "gateway",
-				"metadataServiceIP": "metadata",
+var networkConfigTest = []struct {
+	name       string
+	data       map[string]interface{}
+	wantConfig string
+}{
+	{
+		name: "with static ip",
+		data: map[string]interface{}{
+			"network": &system.Network{
+				DNI: "dni0",
+				StaticIP: &system.StaticIP{
+					Address: "address0",
+					Gateway: "gateway0",
+				},
 			},
-			output: multiDNIs,
+			"instanceIP":        "instanceip",
+			"defaultGateway":    "defaultgateway",
+			"metadataServiceIP": "metaserverip",
 		},
-	}
-	for _, testcase := range testcases {
-		t.Run(testcase.name, func(t *testing.T) {
-			b, err := generateNetworkTemplate(testcase.data)
+		wantConfig: `version = 2
+[dni0.static4]
+addresses = ["address0"]
+[dni0.route]
+to = "default"
+via = "gateway0"
+[eth0.static4]
+addresses = ["instanceip/25"]
+[[eth0.route]]
+to = "metaserverip/32"
+from = "instanceip"
+via = "defaultgateway"
+`,
+	},
+	{
+		name: "with dhcp",
+		data: map[string]interface{}{
+			"network": &system.Network{
+				DNI: "dni0",
+			},
+			"instanceIP":        "instanceip",
+			"defaultGateway":    "defaultgateway",
+			"metadataServiceIP": "metaserverip",
+		},
+		wantConfig: `version = 2
+[dni0]
+dhcp4 = true
+primary = true
+[eth0.static4]
+addresses = ["instanceip/25"]
+[[eth0.route]]
+to = "metaserverip/32"
+from = "instanceip"
+via = "defaultgateway"
+`,
+	},
+}
+
+func TestNetworkConfiguration(t *testing.T) {
+	g := NewWithT(t)
+	for _, tt := range networkConfigTest {
+		t.Run(tt.name, func(t *testing.T) {
+			b, err := system.GenerateNetworkTemplate(tt.data)
 			g.Expect(err).NotTo(HaveOccurred())
-			g.Expect(string(b)).To(Equal(testcase.output))
+			g.Expect(string(b)).To(Equal(tt.wantConfig))
 		})
 	}
 }
