769c5dffa5c9ac63326bf5df751ad4223bcb679b
Joey Wang <jiayiwang7@yahoo.com>
2022-12-05 13:51:02 -0500

    Support multi DNI and static IP for snow (#1627)

--- projects/aws/bottlerocket-bootstrap/pkg/providers/snow/system/network_test.go
+++ projects/aws/bottlerocket-bootstrap/pkg/providers/snow/system/network_test.go
@@ -16,11 +16,29 @@ var networkConfigTest = []struct {
 	{
 		name: "with static ip",
 		data: map[string]interface{}{
-			"network": &system.Network{
-				DNI: "dni0",
-				StaticIP: &system.StaticIP{
-					Address: "address0",
-					Gateway: "gateway0",
+			"network": []system.NetworkMapping{
+				{
+					DNI: "dni0",
+					StaticIP: &system.StaticIP{
+						Address: "address0",
+						Gateway: "gateway0",
+						Primary: true,
+					},
+				},
+				{
+					DNI: "dni1",
+					StaticIP: &system.StaticIP{
+						Address: "address1",
+						Gateway: "gateway1",
+						Primary: false,
+					},
+				},
+				{
+					DNI: "dni2",
+					StaticIP: &system.StaticIP{
+						Address: "address2",
+						Gateway: "gateway2",
+					},
 				},
 			},
 			"instanceIP":        "instanceip",
@@ -28,11 +46,23 @@ var networkConfigTest = []struct {
 			"metadataServiceIP": "metaserverip",
 		},
 		wantConfig: `version = 2
+[dni0]
+primary = true
 [dni0.static4]
 addresses = ["address0"]
 [dni0.route]
 to = "default"
 via = "gateway0"
+[dni1.static4]
+addresses = ["address1"]
+[dni1.route]
+to = "default"
+via = "gateway1"
+[dni2.static4]
+addresses = ["address2"]
+[dni2.route]
+to = "default"
+via = "gateway2"
 [eth0.static4]
 addresses = ["instanceip/25"]
 [[eth0.route]]
@@ -44,8 +74,16 @@ via = "defaultgateway"
 	{
 		name: "with dhcp",
 		data: map[string]interface{}{
-			"network": &system.Network{
-				DNI: "dni0",
+			"network": []system.NetworkMapping{
+				{
+					DNI: "dni0",
+				},
+				{
+					DNI: "dni1",
+				},
+				{
+					DNI: "dni2",
+				},
 			},
 			"instanceIP":        "instanceip",
 			"defaultGateway":    "defaultgateway",
@@ -55,6 +93,10 @@ via = "defaultgateway"
 [dni0]
 dhcp4 = true
 primary = true
+[dni1]
+dhcp4 = true
+[dni2]
+dhcp4 = true
 [eth0.static4]
 addresses = ["instanceip/25"]
 [[eth0.route]]
