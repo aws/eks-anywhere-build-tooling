437f4f980d89ea13a001eca5694ff7ac7fb5f565
Jun Shun Zhang <junshun@amazon.com>
2023-03-15 00:03:01 +0000

    Adding credential-provider-package image/helm build

--- projects/aws/eks-anywhere-packages/Help.mk
+++ projects/aws/eks-anywhere-packages/Help.mk
@@ -11,19 +11,23 @@ clone-repo:  ## Clone upstream `eks-anywhere-packages`
 checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
 
 ##@ Binary Targets
-binaries: ## Build all binaries: `package-manager ecrtokenrefresher` for `linux/amd64 linux/arm64`
+binaries: ## Build all binaries: `package-manager ecrtokenrefresher credentialprovider` for `linux/amd64 linux/arm64`
 _output/bin/eks-anywhere-packages/linux-amd64/package-manager: ## Build `_output/bin/eks-anywhere-packages/linux-amd64/package-manager`
 _output/bin/eks-anywhere-packages/linux-amd64/ecrtokenrefresher: ## Build `_output/bin/eks-anywhere-packages/linux-amd64/ecrtokenrefresher`
+_output/bin/eks-anywhere-packages/linux-amd64/credentialprovider: ## Build `_output/bin/eks-anywhere-packages/linux-amd64/credentialprovider`
 _output/bin/eks-anywhere-packages/linux-arm64/package-manager: ## Build `_output/bin/eks-anywhere-packages/linux-arm64/package-manager`
 _output/bin/eks-anywhere-packages/linux-arm64/ecrtokenrefresher: ## Build `_output/bin/eks-anywhere-packages/linux-arm64/ecrtokenrefresher`
+_output/bin/eks-anywhere-packages/linux-arm64/credentialprovider: ## Build `_output/bin/eks-anywhere-packages/linux-arm64/credentialprovider`
 
 ##@ Image Targets
-local-images: ## Builds `eks-anywhere-packages/images/amd64 ecrtokenrefresher/images/amd64` as oci tars for presumbit validation
-images: ## Pushes `eks-anywhere-packages/images/push ecrtokenrefresher/images/push` to IMAGE_REPO
+local-images: ## Builds `eks-anywhere-packages/images/amd64 ecrtokenrefresher/images/amd64 credentialprovider/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `eks-anywhere-packages/images/push ecrtokenrefresher/images/push credentialprovider/images/push` to IMAGE_REPO
 eks-anywhere-packages/images/amd64: ## Builds/pushes `eks-anywhere-packages/images/amd64`
 ecrtokenrefresher/images/amd64: ## Builds/pushes `ecrtokenrefresher/images/amd64`
+credentialprovider/images/amd64: ## Builds/pushes `credentialprovider/images/amd64`
 eks-anywhere-packages/images/push: ## Builds/pushes `eks-anywhere-packages/images/push`
 ecrtokenrefresher/images/push: ## Builds/pushes `ecrtokenrefresher/images/push`
+credentialprovider/images/push: ## Builds/pushes `credentialprovider/images/push`
 
 ##@ Helm Targets
 helm/build: ## Build helm chart
@@ -32,6 +36,10 @@ helm/push: ## Build helm chart and push to registry defined in IMAGE_REPO.
 ##@ Fetch Binary Targets
 _output/dependencies/linux-amd64/eksd/kubernetes/client: ## Fetch `_output/dependencies/linux-amd64/eksd/kubernetes/client`
 _output/dependencies/linux-arm64/eksd/kubernetes/client: ## Fetch `_output/dependencies/linux-arm64/eksd/kubernetes/client`
+_output/dependencies/linux-amd64/eksa/aws/rolesanywhere-credential-helper: ## Fetch `_output/dependencies/linux-amd64/eksa/aws/rolesanywhere-credential-helper`
+_output/dependencies/linux-arm64/eksa/aws/rolesanywhere-credential-helper: ## Fetch `_output/dependencies/linux-arm64/eksa/aws/rolesanywhere-credential-helper`
+_output/dependencies/linux-amd64/eksa/kubernetes/cloud-provider-aws: ## Fetch `_output/dependencies/linux-amd64/eksa/kubernetes/cloud-provider-aws`
+_output/dependencies/linux-arm64/eksa/kubernetes/cloud-provider-aws: ## Fetch `_output/dependencies/linux-arm64/eksa/kubernetes/cloud-provider-aws`
 
 ##@ Checksum Targets
 checksums: ## Update checksums file based on currently built binaries.
