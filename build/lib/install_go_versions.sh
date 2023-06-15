96ded79baabed550ad53c2a65c01cd5efa6473d7
Jackson West <jgw@amazon.com>
2023-06-15 12:55:56 -0500

    cleans up go version for go-licenses flow

--- build/lib/install_go_versions.sh
+++ build/lib/install_go_versions.sh
@@ -34,18 +34,12 @@ setupgo() {
     ln -sf ${HOME}/sdk/go${version}/bin/gofmt ${GOPATH}/go${majorversion}/bin/gofmt
 }
 
-setupgo "${GOLANG113_VERSION:-1.13.15}"
-setupgo "${GOLANG114_VERSION:-1.14.15}"
-setupgo "${GOLANG115_VERSION:-1.15.15}"
 setupgo "${GOLANG116_VERSION:-1.16.15}"
 setupgo "${GOLANG117_VERSION:-1.17.13}"
 setupgo "${GOLANG118_VERSION:-1.18.10}"
 setupgo "${GOLANG119_VERSION:-1.19.9}"
 setupgo "${GOLANG120_VERSION:-1.20.4}"
 
-# use 1.16 or 1.17 when installing and running go-licenses
-# go-licenses needs to be installed by the same version of go that is being used
-# to generate the deps list during the attribution generation process
 build::common::use_go_version "1.16"
 GOBIN=${GOPATH}/go1.16/bin go install github.com/jaxesn/go-licenses@4497a2a38565e4e6ad095ea8117c25ecd622d0cc
 
@@ -61,5 +55,5 @@ GOBIN=${GOPATH}/go1.19/bin go install github.com/jaxesn/go-licenses@6800d77c11d0
 build::common::use_go_version "1.20"
 GOBIN=${GOPATH}/go1.20/bin go install github.com/jaxesn/go-licenses@6800d77c11d0ef8628e7eda908b1d1149383ca48
 
-# 1.16 is the default so symlink it to /go/bin
-ln -sf ${GOPATH}/go1.16/bin/go-licenses ${GOPATH}/bin
+# 1.18 is the default so symlink it to /go/bin
+ln -sf ${GOPATH}/go1.18/bin/go-licenses ${GOPATH}/bin
