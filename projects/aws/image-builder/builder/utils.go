37cfaf9e17b1eaae7078811e9573094b0b008786
Jackson West <jgw@amazon.com>
2023-07-27 12:53:16 -0500

    Backport image-builder cli fix for getting bundle

--- projects/aws/image-builder/builder/utils.go
+++ projects/aws/image-builder/builder/utils.go
@@ -111,7 +111,7 @@ func execCommand(cmd *exec.Cmd) (string, error) {
 
 func getGitCommitFromBundle(repoPath string) (string, error) {
 	log.Println("Getting git commit from bundle")
-	loadBundleManifestCommandSequence := fmt.Sprintf("source %s/build/lib/eksa_releases.sh && build::eksa_releases::load_bundle_manifest", repoPath, repoPath)
+	loadBundleManifestCommandSequence := fmt.Sprintf("source %s/build/lib/eksa_releases.sh && build::eksa_releases::load_bundle_manifest", repoPath)
 	cmd := exec.Command("bash", "-c", fmt.Sprintf("%s", loadBundleManifestCommandSequence))
 	commandOut, err := execCommand(cmd)
 	if err != nil {
