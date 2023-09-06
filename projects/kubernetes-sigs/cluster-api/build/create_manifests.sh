56cf141899300f514798c9b0e10d018646ef8d15
Tanvir Tatla <tatlat@amazon.com>
2023-09-06 14:39:53 -0700

    fix typo in create_manifests.sh

--- projects/kubernetes-sigs/cluster-api/build/create_manifests.sh
+++ projects/kubernetes-sigs/cluster-api/build/create_manifests.sh
@@ -23,7 +23,7 @@ ARTIFACTS_PATH="$3"
 TAG="$4"
 IMAGE_REPO="$5"
 IMAGE_TAG="$6"
-GOLANG_VERSION="$6"
+GOLANG_VERSION="$7"
 
 MAKE_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
 source "${MAKE_ROOT}/../../../build/lib/common.sh"
