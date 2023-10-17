38cd7c89534e14436c7ae242b4019e3db188b7fd
Vignesh Goutham Ganesh <72776369+vignesh-goutham@users.noreply.github.com>
2023-10-17 22:11:56 +0530

    Fix for eksa_releases overrides (#2570)

--- build/lib/eksa_releases.sh
+++ build/lib/eksa_releases.sh
@@ -41,7 +41,7 @@ function build::eksa_releases::load_bundle_manifest() {
     if [ $dev_release = true ] && [ -n "$EKSA_RELEASE_VERSION" ]; then
       eksa_release_version="$eksa_release_version+build.*"
     fi
-    local -r bundle_manifest_url=$(echo "$release_manifest" | yq e ".spec.releases[] | select(.version == \"$eksa_release_version\") .bundleManifestUrl" -)
+    local bundle_manifest_url=$(echo "$release_manifest" | yq e ".spec.releases[] | select(.version == \"$eksa_release_version\") .bundleManifestUrl" -)
     # EKSA_BUNDLE_MANIFEST_URL is set only when image-builder CLI is running in airgapped mode.
     # This will be set to a filepath that has the downloaded or pre-baked bundles file
     EKSA_BUNDLE_MANIFEST_URL="${EKSA_BUNDLE_MANIFEST_URL:-}"
