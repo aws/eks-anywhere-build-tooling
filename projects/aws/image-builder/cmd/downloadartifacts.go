872953c5c113993f0840aa9cc5efa729793af5f6
Vignesh Goutham Ganesh <vgg@amazon.com>
2023-11-13 10:42:33 -0600

    Fix required flags

--- projects/aws/image-builder/cmd/downloadartifacts.go
+++ projects/aws/image-builder/cmd/downloadartifacts.go
@@ -10,13 +10,6 @@ var downloadArtifactsCmd = &cobra.Command{
 	Short: "Downloads artifacts for airgapped builds",
 	Long:  "Downloads EKS-D and EKS-A artifacts used to build EKS-A Node images in airgapped environment",
 	Run: func(cmd *cobra.Command, args []string) {
-		// Validate Inputs
-		if bo.EKSAReleaseVersion == "" {
-			log.Fatalf("Please set eksa-release flag. EKS-A release versions can be identified from https://anywhere.eks.amazonaws.com/docs/whatsnew/changelog/")
-		}
-		if bo.ReleaseChannel == "" {
-			log.Fatalf("Please set EKS-D release-channel using --release-channel. Can be 1-24, 1-25, 1-26, 1-27 or 1-28")
-		}
 		err := bo.DownloadArtifacts()
 		if err != nil {
 			log.Fatalf("Error downloading manifests: %v", err)
@@ -29,4 +22,10 @@ func init() {
 	downloadArtifactsCmd.Flags().BoolVar(&bo.Force, "force", false, "Force flag to clean up leftover files from previous execution")
 	downloadArtifactsCmd.Flags().StringVar(&bo.ReleaseChannel, "release-channel", "1-27", "EKS-D Release channel for node image. Can be 1-24, 1-25, 1-26, 1-27 or 1-28")
 	downloadArtifactsCmd.Flags().StringVar(&bo.EKSAReleaseVersion, "eksa-release", "", "The EKS-A CLI version to build images for")
+	if err := downloadArtifactsCmd.MarkFlagRequired("eksa-release"); err != nil {
+		log.Fatalf("Please set eksa-release flag. EKS-A release versions can be identified from https://anywhere.eks.amazonaws.com/docs/whatsnew/changelog/")
+	}
+	if err := downloadArtifactsCmd.MarkFlagRequired("release-channel"); err != nil {
+		log.Fatalf("Please set EKS-D release-channel using --release-channel. Can be 1-24, 1-25, 1-26, 1-27 or 1-28")
+	}
 }
