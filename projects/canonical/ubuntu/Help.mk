21f30ee0231fa36589cd3b026f0d21aba40cce23
Abhay Krishna <arnchlm@amazon.com>
2024-04-18 16:41:53 -0700

    Add download/push scripts for Ubuntu RTOS EKS-A image (#3034)

---
+++ projects/canonical/ubuntu/Help.mk
@@ -0,0 +1,51 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ Run in Docker Targets
+run-in-docker/all-attributions: ## Run `all-attributions` in docker builder container
+run-in-docker/all-attributions-checksums: ## Run `all-attributions-checksums` in docker builder container
+run-in-docker/all-checksums: ## Run `all-checksums` in docker builder container
+run-in-docker/attribution: ## Run `attribution` in docker builder container
+run-in-docker/attribution-checksums: ## Run `attribution-checksums` in docker builder container
+run-in-docker/binaries: ## Run `binaries` in docker builder container
+run-in-docker/checksums: ## Run `checksums` in docker builder container
+run-in-docker/clean: ## Run `clean` in docker builder container
+run-in-docker/clean-go-cache: ## Run `clean-go-cache` in docker builder container
+run-in-docker/validate-checksums: ## Run `validate-checksums` in docker builder container
+
+##@ Artifact Targets
+tarballs: ## Create tarballs by calling build/lib/simple_create_tarballs.sh unless SIMPLE_CREATE_TARBALLS=false, then tarballs must be defined in project Makefile
+s3-artifacts: # Prepare ARTIFACTS_PATH folder structure with tarballs/manifests/other items to be uploaded to s3
+upload-artifacts: # Upload tarballs and other artifacts from ARTIFACTS_PATH to S3
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-go-cache: ## Removes the GOMODCACHE AND GOCACHE folders
+
+##@Fetch Binary Targets
+handle-dependencies: ## Download and extract TARs for each dependency listed in PROJECT_DEPENDENCIES
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@Update Helpers
+start-docker-builder: ## Start long lived builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+run-buildkit-and-registry: ## Run buildkitd and a local docker registry as containers
+stop-buildkit-and-registry: ## Stop the buildkitd and a local docker registry containers
+generate: ## Update UPSTREAM_PROJECTS.yaml
+update-go-mods: ## Update locally checked-in go sum to assist in vuln scanning
+update-vendor-for-dep-patch: ## After bumping dep in go.mod file, uses generic vendor update script or one provided from upstream project
+patch-for-dep-update: ## After bumping dep in go.mod file and updating vendor, generates patch
+create-ecr-repos: ## Create repos in ECR for project images for local testing
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `upload-artifacts`
+release: ## Called via prow postsubmit + release jobs, calls `upload-artifacts`
+########### END GENERATED ###########################
