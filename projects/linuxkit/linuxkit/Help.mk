a70f3094dd898b2777e82131c88fca3b43f715c6
Jackson West <jaxesn@gmail.com>
2024-08-30 19:20:02 +0000

    add placeholder to avoid codebuild failures

---
+++ projects/linuxkit/linuxkit/Help.mk
@@ -0,0 +1,65 @@
+
+
+
+########### DO NOT EDIT #############################
+# To update call: make add-generated-help-block
+# This is added to help document dynamic targets and support shell autocompletion
+
+
+##@ GIT/Repo Targets
+clone-repo:  ## Clone upstream `linuxkit`
+checkout-repo: ## Checkout upstream tag based on value in GIT_TAG file
+
+##@ Image Targets
+local-images: ## Builds `linuxkit/images/amd64` as oci tars for presumbit validation
+images: ## Pushes `linuxkit/images/push` to IMAGE_REPO
+linuxkit/images/amd64: ## Builds/pushes `linuxkit/images/amd64`
+linuxkit/images/push: ## Builds/pushes `linuxkit/images/push`
+
+##@ Run in Docker Targets
+run-in-docker/all-attributions: ## Run `all-attributions` in docker builder container
+run-in-docker/all-attributions-checksums: ## Run `all-attributions-checksums` in docker builder container
+run-in-docker/all-checksums: ## Run `all-checksums` in docker builder container
+run-in-docker/attribution: ## Run `attribution` in docker builder container
+run-in-docker/attribution-checksums: ## Run `attribution-checksums` in docker builder container
+run-in-docker/binaries: ## Run `binaries` in docker builder container
+run-in-docker/checksums: ## Run `checksums` in docker builder container
+run-in-docker/clean: ## Run `clean` in docker builder container
+run-in-docker/clean-go-cache: ## Run `clean-go-cache` in docker builder container
+run-in-docker/validate-checksums: ## Run `validate-checksums` in docker builder container
+
+##@ License Targets
+gather-licenses: ## Helper to call $(GATHER_LICENSES_TARGETS) which gathers all licenses
+attribution: ## Generates attribution from licenses gathered during `gather-licenses`.
+attribution-pr: ## Generates PR to update attribution files for projects
+attribution-checksums: ## Update attribution and checksums files.
+all-attributions: ## Update attribution files for all RELEASE_BRANCHes.
+all-attributions-checksums: ## Update attribution and checksums files for all RELEASE_BRANCHes.
+
+##@ Clean Targets
+clean: ## Removes source and _output directory
+clean-go-cache: ## Removes the GOMODCACHE AND GOCACHE folders
+clean-repo: ## Removes source directory
+
+##@Fetch Binary Targets
+handle-dependencies: ## Download and extract TARs for each dependency listed in PROJECT_DEPENDENCIES
+
+##@ Helpers
+help: ## Display this help
+add-generated-help-block: ## Add or update generated help block to document project make file and support shell auto completion
+
+##@Update Helpers
+start-docker-builder: ## Start long lived builder base docker container
+stop-docker-builder: ## Clean up builder base docker container
+run-buildkit-and-registry: ## Run buildkitd and a local docker registry as containers
+stop-buildkit-and-registry: ## Stop the buildkitd and a local docker registry containers
+generate: ## Update UPSTREAM_PROJECTS.yaml
+update-go-mods: ## Update locally checked-in go sum to assist in vuln scanning
+update-vendor-for-dep-patch: ## After bumping dep in go.mod file, uses generic vendor update script or one provided from upstream project
+patch-for-dep-update: ## After bumping dep in go.mod file and updating vendor, generates patch
+create-ecr-repos: ## Create repos in ECR for project images for local testing
+
+##@ Build Targets
+build: ## Called via prow presubmit, calls `github-rate-limit-pre validate-checksums attribution local-images   attribution-pr github-rate-limit-post`
+release: ## Called via prow postsubmit + release jobs, calls `validate-checksums images  `
+########### END GENERATED ###########################
